006f920c9c8f91a112cbdd5ac9c751cb60bfc596
DmitrievaMarina <79178074076@yandex.ru>
2023-04-25 00:43:54 +0500

    lvl1 is DONE

--- lvl_1/task_1.1.py
+++ lvl_1/task_1.1.py
@@ -6,4 +6,4 @@ my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start M
 
 # Выведите на консоль с помощью индексации строки, последовательно: первый трек, последний, второй, второй с конца
 # Нельзя переопределять my_favorite_songs и запятая не должна выводиться.
-print(my_favorite_songs[0:14], my_favorite_songs[-13:],  my_favorite_songs[16:30], my_favorite_songs[-26:-15] )
+print(my_favorite_songs[0:14], my_favorite_songs[-13:],  my_favorite_songs[16:30], my_favorite_songs[-26:-15])
--- lvl_1/task_1.2.py
+++ lvl_1/task_1.2.py
@@ -17,6 +17,12 @@ my_favorite_songs = [
     ['Nowhere to Run', 2.58],
     ['In This World', 4.02],
 ]
+import random
+songs = random.sample(my_favorite_songs, 3)
+summ = (songs[0][1] + songs[1][1] + songs[2][1])
+print(f'Три песни звучат {round(summ, 2)} минут')
+
+
 
 # Пункт B. 
 # Есть словарь песен 
@@ -35,12 +41,28 @@ my_favorite_songs_dict = {
     'In This World': 4.02,
 }
 
+import random
+playlist = list(my_favorite_songs_dict.items())
+random_s = random.sample(playlist, 3)
+playlist = random_s[0][1] + random_s[1][1] + random_s[2][1]
+print(f'Три песни звучат {round(playlist,2)} минут' )
+
 # Дополнительно для пунктов A и B
 # Пункт C.
 # Сгенерируйте случайные песни с помощью модуля random
 # import random
+import random
+random_songs = random.sample(my_favorite_songs, 3)
+print(random_songs[0][0] + ', ' + random_songs[1][0] + ', ' + random_songs[2][0])
+
 
 # Дополнительно 
 # Пункт D.
 # Переведите минуты и секунды в формат времени. Используйте модуль datetime 
+import datetime
+time = datetime.datetime.now()
+print(time.strftime('%M минут %S секунд'))
+
+
+
 
